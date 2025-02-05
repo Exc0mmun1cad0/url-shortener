57b3c2ec2b2b87835ea6954379d60fae0bdd433a
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-02-05 15:56:41 +0500

    install new pkgs

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"url-shortener/internal/config"
+	"url-shortener/internal/storage/postgres"
+)
+
 func main() {
-		
+	cfg := config.MustLoad()
+
+	storage, err := postgres.New(cfg.Postgres)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer storage.Close()
+	_ = storage
 }
