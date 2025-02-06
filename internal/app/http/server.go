12a1f25e130a621c80a57436895331ae53f2569b
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-02-06 21:45:36 +0500

    add function for router creation

--- internal/app/http/server.go
+++ internal/app/http/server.go
@@ -3,30 +3,31 @@ package httpapp
 import (
 	"log/slog"
 	"net/http"
-	"url-shortener/internal/config"
+	"url-shortener/internal/http/handlers/link/save"
+	mw "url-shortener/internal/http/middlewares"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 // NewRouter creates router for our app. It will be used in
-// creating http.Server in handler field.
+// creating http.Server as a handler.
 func NewRouter(
 	log *slog.Logger,
-	config *config.Config,
+	linkSaver save.LinkSaver,
 ) http.Handler {
-	mux := chi.NewMux()
-	addRoutes(
-		mux,
-		log,
-		config,
+	router := chi.NewRouter()
+
+	// Add middlewares for router
+	router.Use(
+		middleware.RequestID,
+		middleware.RealIP,
+		mw.NewLogger(log),
+		middleware.Recoverer,
+		middleware.URLFormat,
 	)
 
-	// TODO: add middleware registration. How should it be:
-	// var handler http.Handler = mux
-	// handler = someMiddleware(handler)
-	// handler = someMiddleware2(handler)
-	// handler = someMiddleware3(handler)
-	// return handler
+	addRoutes(router, log, linkSaver)
 
-	return mux
+	return router
 }
