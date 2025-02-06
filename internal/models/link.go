21ffa9ae1fb43911baa5ffcf6f5fc377a1087dea
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-02-06 20:34:44 +0500

    add json struct tags for unmarshalling

--- internal/models/link.go
+++ internal/models/link.go
@@ -4,8 +4,8 @@ import "time"
 
 // Link is a model for url representation in storage
 type Link struct {
-	ID        uint       `db:"link_id"`
-	Alias     string    `db:"alias"`
-	RawURL    string    `db:"raw_url"`
-	CreatedAt time.Time `db:"created_at"`
+	ID        uint       `db:"link_id" json:"id"`
+	Alias     string    `db:"alias" json:"alias"`
+	RawURL    string    `db:"raw_url" json:"raw_url"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
 }
