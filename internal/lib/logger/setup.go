07bee8cc7f5fdfed4e7d4ede0696798dd36c5da6
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-02-22 14:30:39 +0300

    refactor setting up logger

--- internal/lib/logger/setup.go
+++ internal/lib/logger/setup.go
@@ -1,58 +1,36 @@
 package logger
 
 import (
-	"fmt"
-	"log"
 	"log/slog"
 	"os"
-	"time"
 	"url-shortener/pkg/badaslog"
 )
 
 const (
-	envDev  = "dev"
-	envProd = "prod"
-
-	layout = "2006-01-02 15:04:05.000"
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func SetupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 
 	switch env {
-	case envDev:
-		// log = slog.New(
-		// 	slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug, AddSource: true, ReplaceAttr: formatTime}),
-		// )
+	case envLocal:
 		log = slog.New(badaslog.NewHandler(nil))
-
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
 	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
 		)
 	}
 
 	return log
-}
-
-// formatTime changes time representation in "time" record attribute
-func formatTime(groups []string, a slog.Attr) slog.Attr {
-	if a.Key == slog.TimeKey {
-		oldTimeVal := a.Value.String()[:len(layout)]
-
-		oldTime, err := time.Parse(layout, oldTimeVal)
-		if err != nil {
-			log.Fatalf("cannot parse time attribute: %s", err.Error())
-		}
-
-		newTime := fmt.Sprintf(
-			"{%d:%d:%d}:[%d:%d:%d.%d]",
-			oldTime.Day(), oldTime.Month(), oldTime.Year(),
-			oldTime.Hour(), oldTime.Minute(), oldTime.Second(), oldTime.Nanosecond()/1e7,
-		)
-
-		a.Value = slog.StringValue(newTime)
-	}
-
-	return a
 }
